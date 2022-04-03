e2b1f4b753ca3756e0c70821e3c5c3efbf1d0108
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-04-04 01:21:44 +0530

    feat: Add Watch Later Page

--- src/pages/Liked/Liked.jsx
+++ src/pages/Liked/Liked.jsx
@@ -6,8 +6,8 @@ const Liked = () => {
             <div>
                   <Header />
                   
-                  <div className="playlist_wrapper">
-                        <div className="playlist_container flex flex_col flex_justify_center flex_align_center">
+                  <div className="list_wrapper">
+                        <div className="list_container flex flex_col flex_justify_center flex_align_center">
                               <i className="fa-solid fa-thumbs-up music_icon"></i>
                               <h3 className="playlist_title">No Liked Video</h3>
                               <i className="fa-solid fa-plus add_btn"></i>
--- src/pages/WatchLater/WatchLater.jsx
+++ src/pages/WatchLater/WatchLater.jsx
@@ -3,8 +3,16 @@ import { Header} from "../../components";
 
 const WatchLater = () => {
       return (
-            <div className="history_container">
+            <div>
                   <Header />
+                  
+                  <div className="list_wrapper">
+                        <div className="list_container flex flex_col flex_justify_center flex_align_center">
+                              <i className="fa-solid fa-clock music_icon"></i>
+                              <h3 className="playlist_title">Empty Watch Later List</h3>
+                              <i className="fa-solid fa-plus add_btn"></i>
+                        </div>
+                  </div>
             </div>
       );
 }
