90fb82de3e8e71b86c9f454766fec16c0f41f39a
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-04-04 01:30:33 +0530

    feat: Add History Page

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -4,9 +4,16 @@ import { Header} from "../../components";
 
 const History = () => {
       return (
-            <div className="history_container">
+            <div>
                   <Header />
                   
+                  <div className="list_wrapper">
+                        <div className="list_container flex flex_col flex_justify_center flex_align_center">
+                              <i className="fa-solid fa-clock music_icon"></i>
+                              <h3 className="playlist_title">No History</h3>
+                              <button className="nav_categories">Start watching</button>
+                        </div>
+                  </div>
             </div>
       )
 }
