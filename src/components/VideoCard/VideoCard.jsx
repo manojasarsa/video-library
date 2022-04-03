f5b56a235b0de83c32d9e9817f57263e695bbc43
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-04-04 05:28:07 +0530

    fix: fixed deploy errors

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -3,39 +3,10 @@ import * as FaIcons from "react-icons/fa";
 
 const VideoCard = ({video}) => {
 
-      const {  title, description, vidImage, creator, category } = video;
+      const {  title, vidImage, creator } = video;
 
       return (
-            // <div className="card_wrapper flex ">
-            //       <div className="card_container flex flex_col">
-            //             <div>
-            //                   <img className="card_img img_responsive adjust_image" src= {vidImage} alt= {title} />
-            //             </div>
-            //             <div className="card_details_box flex ">
-
-            //                   <img className="avatar_img" src= {vidImage} alt= {title} />
-
-            //                   <div className="video_details_box ">
-            //                         <div className="card_title"> {title} </div>
-                                                                
-            //                         <div className="channel_box flex flex_justify_between">
-
-            //                               <div className="creator_box flex">
-            //                                     <div className="channel_name"> {creator} </div>
-            //                                     <FaIcons.FaCheckCircle className="verify_icon" />
-            //                               </div>
-                                          
-            //                               <div className="nav_right flex flex_justify_between flex_align_center">
-            //                                     <FaIcons.FaThumbsUp className="sidebar_icons" />
-            //                                     <FaIcons.FaClock className="sidebar_icons" />
-            //                                     <FaIcons.FaFolderPlus className="sidebar_icons" />
-            //                               </div>
-            //                         </div>
-            //                   </div>  
-            //             </div>
-            //       </div>
-            // </div>
-
+            
             <div className="card_image flex">
                   <div className="card_vertical">
                         <div className="card_vertical_info flex flex_col flex_justify_start">
