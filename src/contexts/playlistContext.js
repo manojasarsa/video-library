a1b24960c567d4174710bc89e7efe5b9fd5f000b
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-04-07 13:44:13 +0530

    refactor: Add playlistReducer to reducer folder

--- src/contexts/playlistContext.js
+++ src/contexts/playlistContext.js
@@ -2,43 +2,9 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 import { createContext, useReducer, useContext, useEffect } from "react";
 import { useAuth } from "./authContext";
-// import { playlistReducer } from "../reducer/playlistReducer";
+import { playlistReducer } from "../reducer/playlistReducer";
 
 const PlaylistContext = createContext();
-
-const playlistReducer = (state, action) => {
-
-	switch (action.type) {
-
-            case "GET_PLAYLISTS":
-                  return {
-                        ...state,
-                        playlistsItems: action.payload,
-                  };
-            case "CREATE_PLAYLIST":
-                  return {
-                        ...state,
-                        playlistsItems: action.payload,
-                  };
-            case "DELETE_PLAYLIST":
-                  return {
-                        ...state,
-                        playlistsItems: action.payload,
-                  };
-            case "ADD_VIDEO_TO_PLAYLIST":
-                  return {
-                        ...state,
-                        playlistsItems: state.playlistsItems.map((playlist) => playlist._id === action.payload._id ? action.payload : playlist) 
-                  };
-            case "DELETE_VIDEO_FROM_PLAYLIST":
-                  return {
-                        ...state,
-                        playlistsItems: state.playlistsItems.map((playlist) => playlist._id === action.payload._id ? action.payload : playlist)
-                  };
-		default:
-			return state;
-	}
-};
 
 const PlaylistProvider = ({ children }) => {
       
