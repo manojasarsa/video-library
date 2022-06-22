e2f2d73bae3f3de7046344be6a77eb5e9736ec9b
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-06-22 11:33:27 +0530

    refactor: change title name

--- src/App.js
+++ src/App.js
@@ -3,17 +3,6 @@ import { NavRoutes } from "./Routes/NavRoutes";
 
 function App() {
 
-    // const [loaderValue, setLoaderValue] = useState(true);
-
-    // const preload = document.getElementById('preloader');
-
-    // if (preload) {
-    //     setTimeout(() => {
-    //         preload.style.display = "none";
-    //         setLoaderValue(false);
-    //     }, 2000);
-    // }   
-
     return (
         <div className="App">
            <NavRoutes />
--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -35,27 +35,7 @@ const VideoCard = ({ video }) => {
 
     const playlistHandler = () => {
         createPlaylist(playlistName, video);
-        // if (x) {
-        //     let lengthOfPlaylists = playlistState.playlistsItems.length ;
-        //     addVideoToPlaylist( playlistState.playlistsItems[lengthOfPlaylists]._id, video);
-        // }
     }
-    
-    // const addVideoToPlaylistHandler = () => {
-    //     const lengthOfPlaylists = playlistState.playlistsItems.length - 1;
-    //     const timer = setTimeout(() => {
-    //         let x = playlistState.playlistsItems.map((p) => console.log(p));
-
-    //     }, 3000);
-    //     addVideoToPlaylist( playlistState.playlistsItems[lengthOfPlaylists]._id, video);
-    // }
-
-    // useEffect(() => {
-
-
-    //         addVideoToPlaylist((playlistState.playlistsItems[playlistState.playlistsItems.length - 1])?._id, video);
-        
-    // }, [playlistState.playlistsItems.length]);
 
     const modalHandler = () => setPlaylistModal(true);
 
@@ -159,9 +139,9 @@ const VideoCard = ({ video }) => {
 
                                                             ? (
                                                                 <div className="playlist_list_btns flex flex_align_center">
-                                                                    <i 
+                                                                    <i
                                                                         className="fa-solid playlist_one fa-check icon_circle_plus"
-                                                                        onClick={() => 
+                                                                        onClick={() =>
                                                                             deleteVideoFromPlaylist(eachPlaylist._id, video._id)
                                                                         }>
                                                                     </i>
@@ -172,11 +152,11 @@ const VideoCard = ({ video }) => {
 
                                                             ) : (
                                                                 <div className="playlist_list_btns flex flex_align_center">
-                                                                    <i  
+                                                                    <i
                                                                         className="fa-solid fa-plus playlist_one icons icon_circle_plus"
                                                                         key={eachPlaylist._id}
-                                                                        onClick={() => 
-                                                                            addVideoToPlaylist(eachPlaylist._id, video) 
+                                                                        onClick={() =>
+                                                                            addVideoToPlaylist(eachPlaylist._id, video)
                                                                         }>
                                                                     </i>
                                                                     <span className="play_title">{eachPlaylist.title}</span>
@@ -192,17 +172,17 @@ const VideoCard = ({ video }) => {
                                                             type="text"
                                                             className="input"
                                                             placeholder="Enter Playlist Name"
-                                                            onChange={(e) => 
+                                                            onChange={(e) =>
                                                                 setPlaylistName((prev) => ({
                                                                     ...prev,
                                                                     playlist: e.target.value,
                                                                 }))
-                                                            } 
+                                                            }
                                                         />
                                                     </div>
 
                                                 </div>
-                                                <button className="create_playlist_btn flex flex_justify_center" onClick={() => 
+                                                <button className="create_playlist_btn flex flex_justify_center" onClick={() =>
                                                     playlistHandler()
                                                 }>
                                                     <i class="fa-solid fa-circle-check icon_circle_plus">
