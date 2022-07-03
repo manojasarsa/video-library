be5e7fab6188986614bd30b2a1d44eed44850f1e
Manoj Asarsa <manojasarsa7611@gmail.com>
2022-07-03 21:00:20 +0530

    refactor: import toast.css file globally

--- src/contexts/watchLaterContext.js
+++ src/contexts/watchLaterContext.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+// import "react-toastify/dist/ReactToastify.css";
 import { createContext, useReducer, useContext, useEffect } from "react";
 import { useAuth } from "./authContext";
 import { watchLaterReducer } from "../reducer/watchLaterReducer";
